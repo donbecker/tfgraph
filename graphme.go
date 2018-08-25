93174dfed287ff486e1c3a77ded66a34e84c5644
donbecker <donbecker@donbeckeronline.com>
2018-08-25 10:44:59 -0700

    add execution note

--- graphme.go
+++ graphme.go
@@ -3,6 +3,10 @@
 // `terraform graph | dot -Tsvg > graph.svg`
 // what this file should do
 // `terraform graph | graphprocess.go | dot -Tsvg > graph.svg`
+
+// Execution from vscode console
+// go run graphme.go graphprocess.go
+
 package main
 
 import (
