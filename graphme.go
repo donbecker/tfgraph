498467ebb8a4c9405abdd2cc2af5a55758dec30d
donbecker <donbecker@donbeckeronline.com>
2018-08-25 10:25:55 -0700

    clean up comments in go file

--- graphme.go
+++ graphme.go
@@ -1,3 +1,5 @@
-* Example Usage
-* `terraform graph | dot -Tsvg > graph.svg`
-* ample command with new graph: `terraform graph | graphprocess.go | dot -Tsvg > graph.svg`
+// Example Usage
+// normal command line (without this file)
+// `terraform graph | dot -Tsvg > graph.svg`
+// what this file should do
+// `terraform graph | graphprocess.go | dot -Tsvg > graph.svg`
